26093589ae3fba48c0ff5232aeffde922c7240b3
MShoaibSadiq845 <shoaib_sadiq_intern@netixsol.com>
2026-08-28 07:45:52 -0700

    ok

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://chatbot-six-drab-61.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
